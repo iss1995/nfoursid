running but with wrong results

src/nfoursid/nfoursid.py
@@ -40,14 +40,14 @@ class NFourSID:
         self.y_dim = self.y_array.shape[1]
 
     def subspace_identification(self):
-        u_hankel = Utils.block_hankel_matrix(self.u_array, self.num_block_rows).tocsr()
-        y_hankel = Utils.block_hankel_matrix(self.y_array, self.num_block_rows).tocsr()
+        u_hankel = Utils.block_hankel_matrix_parallel(self.u_array, self.num_block_rows).tocsr()
+        y_hankel = Utils.block_hankel_matrix_parallel(self.y_array, self.num_block_rows).tocsr()
 
         u_past, u_future = u_hankel[:, :-self.num_block_rows], u_hankel[:, self.num_block_rows:]
         y_past, y_future = y_hankel[:, :-self.num_block_rows], y_hankel[:, self.num_block_rows:]
-        u_instrumental_y = sparse.hstack([u_future, u_past, y_past, y_future])
+        u_instrumental_y = sparse.vstack([u_future, u_past, y_past, y_future])
 
-        q, r = np.linalg.qr(u_instrumental_y.toarray().T, mode='reduced')  # QR on dense array
+        q, r = np.linalg.qr(u_instrumental_y.toarray(), mode='reduced')  # QR on dense array
 
         y_rows, u_rows = self.y_dim * self.num_block_rows, self.u_dim * self.num_block_rows
         self.R32 = r[-y_rows:, u_rows:-y_rows]
@@ -65,8 +65,71 @@ class NFourSID:
 
         return self._identify_state_space(observability_decomposition)
 
-    def _identify_state_space(self, observability_decomposition: Decomposition) -> Tuple[StateSpace, np.ndarray]:
+    def apply_n4sid(self, rank: int = None) -> Tuple[StateSpace, np.ndarray]:
+        if rank is None:
+            rank = self.y_dim * self.num_block_rows
+
+        self.x_dim = rank
+
+        observability_decomposition = self._apply_observability_decomposition()
+
+        covariance_matrix, abcd = self._calculate_covariance_matrix(observability_decomposition)
+
+        q = covariance_matrix[:self.x_dim, :self.x_dim]
+        r = covariance_matrix[self.x_dim:, self.x_dim:]
+        s = covariance_matrix[:self.x_dim, self.x_dim:]
+        state_space_covariance_matrix = np.block([
+            [r, s.T],
+            [s, q]
+        ])
+        return (
+            StateSpace(
+                abcd[:self.x_dim, :self.x_dim],
+                abcd[:self.x_dim, self.x_dim:],
+                abcd[self.x_dim:, :self.x_dim],
+                abcd[self.x_dim:, self.x_dim:],
+            ),
+            (state_space_covariance_matrix + state_space_covariance_matrix.T) / 2
+        )
+
+    def _apply_observability_decomposition(self):
+        u_hankel = Utils.block_hankel_matrix_parallel(self.u_array, self.num_block_rows).tocsr()
+        y_hankel = Utils.block_hankel_matrix_parallel(self.y_array, self.num_block_rows).tocsr()
+
+        u_past, u_future = u_hankel[:, :-self.num_block_rows], u_hankel[:, self.num_block_rows:]
+        y_past, y_future = y_hankel[:, :-self.num_block_rows], y_hankel[:, self.num_block_rows:]
+        # u_instrumental_y = sparse.hstack([u_future, u_past, y_past, y_future])
+
+        # approximate QR with Cholesky
+        _, r = Utils.sparse_qr(sparse.hstack([u_future, u_past, y_past, y_future]).T, mode='NATURAL')
+
+        y_rows, u_rows = self.y_dim * self.num_block_rows, self.u_dim * self.num_block_rows
+
+        R22 = r[u_rows:-y_rows, u_rows:-y_rows]
+        R32 = r[-y_rows:, u_rows:-y_rows]
+
+        u_and_y = sparse.hstack([u_hankel, y_hankel])
+        observability = R32 @ sparse.linalg.pinv(R22 @ u_and_y)
+        observability_decomposition = Utils.reduce_decomposition(
+            Utils.eigenvalue_decomposition(observability),
+            self.x_dim
+        )
+
+        return observability_decomposition
+
+    def _calculate_covariance_matrix(self, observability_decomposition: Decomposition) -> np.ndarray:
         x = (np.sqrt(observability_decomposition.s.diagonal()) @ observability_decomposition.vh)[:, :-1]
+        last_y, last_u = self.y_array[self.num_block_rows:, :].T, self.u_array[self.num_block_rows:, :].T
+        x_and_y = np.concatenate([x[:, 1:], last_y[:, :-1]])
+        x_and_u = np.concatenate([x[:, :-1], last_u[:, :-1]])
+        abcd = np.linalg.pinv(x_and_u @ x_and_u.T) @ x_and_u @ x_and_y.T
+        abcd = abcd.T
+        residuals = x_and_y - abcd @ x_and_u
+        covariance_matrix = residuals @ residuals.T / residuals.shape[1]
+        return covariance_matrix, abcd
+
+    def _identify_state_space(self, observability_decomposition: Decomposition) -> Tuple[StateSpace, np.ndarray]:
+        x = (np.sqrt(observability_decomposition.s) @ observability_decomposition.vh)[:, :-1]
         last_y, last_u = self.y_array[self.num_block_rows:, :].T, self.u_array[self.num_block_rows:, :].T
         x_and_y = np.concatenate([x[:, 1:], last_y[:, :-1]])
         x_and_u = np.concatenate([x[:, :-1], last_u[:, :-1]])
@@ -92,10 +155,11 @@ class NFourSID:
         )
 
     def _get_observability_matrix_decomposition(self) -> Decomposition:
-        u_hankel = Utils.block_hankel_matrix(self.u_array, self.num_block_rows).tocsr()
-        y_hankel = Utils.block_hankel_matrix(self.y_array, self.num_block_rows).tocsr()
-        u_and_y = sparse.hstack([u_hankel, y_hankel])
-        observability = self.R32 @ sparse.linalg.pinv(self.R22 @ u_and_y)
+        u_hankel = Utils.block_hankel_matrix_parallel(self.u_array, self.num_block_rows).tocsr()
+        y_hankel = Utils.block_hankel_matrix_parallel(self.y_array, self.num_block_rows).tocsr()
+        u_and_y = sparse.vstack([u_hankel, y_hankel])
+        observability = self.R32 @ np.linalg.pinv(self.R22)
+        observability = sparse.csr_matrix(observability) @ u_and_y
         observability_decomposition = Utils.reduce_decomposition(
             Utils.eigenvalue_decomposition(observability),
             self.x_dim
@@ -105,7 +169,8 @@ class NFourSID:
     def plot_eigenvalues(self, ax: plt.Axes):
         if self.R32_decomposition is None:
             raise Exception('Perform subspace identification first.')
-        ax.semilogy(np.diag(self.R32_decomposition.s), 'x')
+
+        ax.semilogy(np.diag(self.R32_decomposition.s.toarray()), 'x')
         ax.set_title('Estimated observability matrix decomposition')
         ax.set_xlabel('Index')
         ax.set_ylabel('Eigenvalue')

src/nfoursid/utils.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 import numpy as np
+from multiprocessing import Pool, cpu_count
 from scipy import sparse
 from scipy.sparse.linalg import svds
 
@@ -9,6 +10,9 @@ class Decomposition:
         self.u = u
         self.s = s
         self.vh = vh
+
+    def to_array(self):
+        self.u, self.s, self.vh = self.u.toarray(), self.s.toarray(), self.vh.toarray()
 
 
 class Utils:
@@ -45,12 +49,13 @@ class Utils:
         Reduce an eigenvalue decomposition ``decomposition`` such that only ``rank`` number of biggest eigenvalues
         remain. Returns another ``Decomposition`` adapted for sparse matrix operations.
         """
-        u, s, vh = decomposition.u, decomposition.s, decomposition.vh
+        u, s, vh = decomposition.u, decomposition.s.toarray(), decomposition.vh
         return Decomposition(
             u[:, :rank],
             s[:rank, :rank],
             vh[:rank, :]
         )
+    
     @staticmethod
     def block_hankel_matrix(
             matrix: np.ndarray,
@@ -76,23 +81,50 @@ class Utils:
         return hankel.tocsr()
 
     @staticmethod
-    def vectorize(matrix: np.ndarray) -> np.ndarray:
-        """
-        Given a matrix ``matrix`` of shape ``(a, b)``, return a vector of shape ``(a*b, 1)`` with all columns of
-        ``matrix`` stacked on top of each other.
-        """
-        return matrix.flatten(order='F').reshape(-1, 1)
+    def fill_hankel_slice(args):
+        matrix, start, end, num_block_rows = args
+        hankel_slice = sparse.lil_matrix((num_block_rows * matrix.shape[1], end - start))
+        for block_row_index in range(end - start):
+            flattened_block_rows = matrix[block_row_index+start:block_row_index+start+num_block_rows, :].flatten()
+            hankel_slice[:, block_row_index] = flattened_block_rows.flatten()
+        return hankel_slice
 
     @staticmethod
-    def unvectorize(vector: np.ndarray, num_rows: int) -> np.ndarray:
-        """
-        Given a vector ``vector`` of shape ``(num_rows*b, 1)``, return a matrix of shape ``(num_rows, b)`` such that
-        the stacked columns of the returned matrix equal ``vector``.
-        """
-        if vector.shape[0] % num_rows != 0 or vector.shape[1] != 1:
-            raise ValueError(f'Vector shape {vector.shape} and `num_rows`={num_rows} are incompatible')
-        return vector.reshape((num_rows, -1), order='F')
+    def block_hankel_matrix_parallel(matrix: np.ndarray, num_block_rows: int) -> sparse.csr_matrix:
+        hankel_rows_dim = num_block_rows * matrix.shape[1]
+        hankel_cols_dim = matrix.shape[0] - num_block_rows + 1
 
+        # Split the task into slices
+        num_slices = cpu_count() - 1
+        slice_size = hankel_cols_dim // num_slices
+        slices = [(matrix, i*slice_size, (i+1)*slice_size, num_block_rows) for i in range(num_slices)]
+        slices[-1] = (matrix, (num_slices-1)*slice_size, hankel_cols_dim, num_block_rows)  # Make sure the last slice goes to the end
+
+        # Create a multiprocessing Pool and fill each slice in parallel
+        with Pool() as p:
+            hankel_slices = p.map(Utils.fill_hankel_slice, slices)
+
+        # Concatenate the slices to form the full Hankel matrix
+        hankel = sparse.hstack(hankel_slices).tocsr()
+
+        return hankel
+
+    @staticmethod
+    def split_and_apply_block_hankel_matrix(matrix: np.ndarray, num_groups: int, num_block_rows: int):
+
+        # Split the matrix into groups
+        matrix_data = DataMatrix(matrix, chunks=num_groups, hankel_block_rows=num_block_rows)
+        groups = matrix_data.split()
+        matrix_data.reset_data(np.asarray([])[None, None])
+
+        # Create a multiprocessing Pool and apply the function to each group in parallel
+        with Pool(processes=num_groups) as p:
+            results = p.map(matrix_data, groups)
+
+        # Concatenate the results into a single matrix
+        results = sparse.hstack(results)
+
+        return results
 
     @staticmethod
     def vectorize(
@@ -117,3 +149,58 @@ class Utils:
             raise ValueError(f'Vector shape {vector.shape} and `num_rows`={num_rows} are incompatible')
         b = vector.shape[0] // num_rows
         return sparse.csr_matrix(vector.toarray().reshape((num_rows, b), order='F'))
+
+    @staticmethod
+    def sparse_qr(matrix: sparse.spmatrix, mode='NATURAL'):
+        """
+        Perform QR decomposition on a sparse matrix using direct sparse operations.
+
+        Parameters:
+        - matrix (sparse.spmatrix): A sparse matrix.
+
+        Returns:
+        - Q (sparse.spmatrix): Orthogonal matrix in sparse format.
+        - R (sparse.spmatrix): Upper triangular matrix in sparse format.
+        """
+        if not sparse.isspmatrix_csc(matrix):
+            matrix = matrix.tocsc()
+
+        # Direct sparse QR decomposition using SuiteSparseQR, if available
+        try:
+            from sksparse.cholmod import cholesky
+            factorization = cholesky(matrix.T.dot(matrix))
+            R = factorization.L().T
+            Q = sparse.spsolve(R, matrix.T, permc_spec=mode).T
+        except ImportError:
+            raise ImportError("The required library scikit-sparse is not installed. "
+                              "Please install it to use this QR decomposition.")
+
+        Q = sparse.csr_matrix(Q)
+        R = sparse.csr_matrix(R)
+
+        return Q, R
+
+
+class DataMatrix:
+
+    def __init__(self, data: np.ndarray, chunks=1, hankel_block_rows=1):
+        self.data = data
+        self.chunks = chunks
+        self.num_rows = data.shape[0]
+        self.num_cols = data.shape[1]
+        self.hankel_block_rows = hankel_block_rows
+
+    def __call__(self, data) -> sparse.csr_matrix:
+        return Utils.block_hankel_matrix(data, self.hankel_block_rows)
+
+    def __len__(self):
+        return self.num_rows
+
+    def split(self):
+        chunk_size = self.num_rows // self.chunks
+        return [self.data[i*chunk_size:(i+1)*chunk_size, :] for i in range(self.chunks)]
+
+    def reset_data(self, data):
+        self.data = data
+        self.num_rows = data.shape[0]
+        self.num_cols = data.shape[1]